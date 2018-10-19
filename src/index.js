309048b2d21b21b3d52dfaf4f27b25d347d9ee81
Paul Ho <hmj1026@gmail.com>
2018-10-19 18:35:04 +0800

    Added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import $ from 'jquery'
+import Popper from 'popper.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import './asset/css/default.css'
 import App from './App'
 
-ReactDOM.render( <App /> , document.getElementById('app'))
+ReactDOM.render( <App /> , document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path')
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.[hash].js'
     },
     module: {
         rules: [
@@ -17,18 +17,38 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/,
-                use: 'css-loader'
+                test: /\.s?css$/,
+                // exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                loader: "url-loader?prefix=font/&limit=5000"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader?limit=10000&mimetype=application/octet-stream"
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+            }
         ]
     },
-    // plugins: [
-    //     new HtmlWebpackPlugin({
-    //         template: `${__dirname}/main.html`,
-    //         filename: 'main.html',
-    //         inject: 'body',
-    //     })
-    // ],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'dist/index.html'),
+            filename: 'index.html'
+        })
+    ],
     devtool: "eval-source-map",
     devServer: {
         contentBase: path.resolve(__dirname, "dist")
